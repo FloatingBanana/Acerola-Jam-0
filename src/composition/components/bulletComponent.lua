b8868d42c6bd437c1b33ed943dce641083e7b0b6
FloatingBanana <thalesmaiademoraes13@gmail.com>
2024-03-06 09:13:46 -0300

    Add lifetime limit to bullets

--- src/composition/components/bulletComponent.lua
+++ src/composition/components/bulletComponent.lua
@@ -16,6 +16,8 @@ function BulletComponent:new(direction, damage)
     self.direction = direction
     self.damage = damage
     self.speed = 500
+
+    self._destroyCounter = 3
 end
 
 function BulletComponent:update(dt)
@@ -24,19 +26,21 @@ function BulletComponent:update(dt)
     body.collisionFilter = bulletFilter
     body:move(self.direction * self.speed * dt)
 
-    if #body.collisions > 0 then
+    self._destroyCounter = self._destroyCounter - dt
+
+    if self._destroyCounter <= 0 then
         CompositionManager.removeEntity(self.entity)
     end
 end
 
 function BulletComponent:onBodyCollision(col, moveOffset)
-    if self.entity == col.item then
-        local otherDamageable = col.other:getComponent("DamageableComponent") --[[@as DamageableComponent]]
+    local otherDamageable = col.other:getComponent("DamageableComponent") --[[@as DamageableComponent]]
 
-        if otherDamageable then
-            otherDamageable:takeDamage(self.damage)
-        end
+    if otherDamageable then
+        otherDamageable:takeDamage(self.damage)
     end
+
+    self._destroyCounter = 0
 end
 
 return BulletComponent
