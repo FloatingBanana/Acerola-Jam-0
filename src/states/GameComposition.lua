33598e956cdf88d1db7fda9cdb6c43afac1aa9b6
FloatingBanana <thalesmaiademoraes13@gmail.com>
2024-03-09 11:35:55 -0300

    Added basic game loop

--- src/states/GameComposition.lua
+++ src/states/GameComposition.lua
@@ -4,6 +4,7 @@ local Bump = require "libs.bump"
 local Vector2 = require "engine.math.vector2"
 local Camera = require "engine.misc.camera"
 local Easing = require "engine.math.easing"
+local Utils = require "engine.misc.utils"
 
 CompositionManager = require "engine.composition.compositionManager"
 EntityBuilder = require "composition.entityBuilder"
@@ -11,19 +12,22 @@ EntityBuilder = require "composition.entityBuilder"
 local world = Bump.newWorld(32)
 local camera = Camera(Vector2(WIDTH/2, 0), 1)
 local player = nil ---@type Entity
-
 local maxHeight = 0
+
+local enemySpawn = -500
 
 function Game:enter()
     CompositionManager.clear()
 
+    maxHeight = 0
+    enemySpawn = -500
+    camera.actualPosition.y = 0
     camera:setInterpolation(Easing.cubic, 30)
-    player = EntityBuilder.player(world, Vector2(100, 100), camera)
+    player = EntityBuilder.player(world, Vector2(WIDTH / 2, HEIGHT - 300), camera)
+
+    player:getComponent("BodyComponent").velocity.y = -700
 
     CompositionManager.addEntity(player)
-    CompositionManager.addEntity(EntityBuilder.enemy(world, Vector2(350, 200)))
-    CompositionManager.addEntity(EntityBuilder.wall(world, Vector2(50, 200), Vector2(300, 32)))
-    CompositionManager.addEntity(EntityBuilder.wall(world, Vector2(280, 400), Vector2(300, 32)))
 end
 
 function Game:draw()
@@ -33,17 +37,34 @@ function Game:draw()
     CompositionManager.broadcastToAllComponents("draw")
 
     camera:detach()
+
+    Utils.setFont(15)
+    love.graphics.print(("Height: %d"):format(-maxHeight), 0, 40)
 end
 
 function Game:update(dt)
     CompositionManager.broadcastToAllComponents("update", dt)
     camera:update(dt)
 
-    maxHeight = math.min(maxHeight, (player:getComponent("Transform2dComponent") --[[@as Transform2dComponent]]).position.y)
+    local bounds = camera:getBounds()
+
+
+    local playerTransform = player:getComponent("Transform2dComponent") --[[@as Transform2dComponent]]
+    local playerDamageable = player:getComponent("DamageableComponent") --[[@as DamageableComponent]]
+
+    maxHeight = math.min(maxHeight, playerTransform.position.y + HEIGHT/4)
     camera.position.y = maxHeight
 
-    if player:getComponent("DamageableComponent").health <= 0 then
+    if playerDamageable.health <= 0 then
         Game:enter()
+    end
+
+
+    if maxHeight < enemySpawn then
+        local enemyPos = Vector2(math.random(0, WIDTH-32), bounds.topLeft.y - 32)
+        CompositionManager.addEntity(EntityBuilder.enemy(world, enemyPos, player, camera))
+
+        enemySpawn = enemySpawn - 500
     end
 end
 
