6ab78795e16007b3803b4109a388c9028009c190
FloatingBanana <thalesmaiademoraes13@gmail.com>
2024-03-12 16:56:05 -0300

    Added input requirement to start the game

--- src/states/GameComposition.lua
+++ src/states/GameComposition.lua
@@ -3,6 +3,7 @@ local Game = {}
 local Bump    = require "libs.bump"
 local Vector2 = require "engine.math.vector2"
 local Camera  = require "engine.misc.camera"
+local Timer   = require "engine.misc.timer"
 local Easing  = require "engine.math.easing"
 local Utils   = require "engine.misc.utils"
 local Lume    = require "engine.3rdparty.lume"
@@ -18,6 +19,8 @@ local screenCanvas = love.graphics.newCanvas(WIDTH, HEIGHT, {format = "rgba16f"}
 local world = Bump.newWorld(32)
 local camera = Camera(Vector2(WIDTH/2, 0), 1)
 local player = nil ---@type Entity
+local uiFadein = 0
+
 
 ---@class GameData
 local gameData = {
@@ -30,19 +33,34 @@ local gameData = {
     gameStarted = false
 }
 
+
+
+local startTimer = Timer(0, 2, false)
+startTimer.onEndedEvent:addCallback(function()
+    gameData.gameStarted = true
+
+    player = EntityBuilder.player(world, Vector2(WIDTH / 2, HEIGHT - 300), camera)
+    CompositionManager.addEntity(player)
+
+    ;(player:getComponent("BodyComponent") --[[@as BodyComponent]]).velocity.y = -700
+end)
+
+
 function Game:enter()
     CompositionManager.clear()
 
     gameData.maxHeight = 0
     gameData.enemySpawn = -500
+    gameData.timeSpeed = 1
+    gameData.targetTimeSpeed = 1
+    gameData.gameStarted = false
+
+    uiFadein = 0
 
     camera.actualPosition.y = 0
     camera:setInterpolation(Easing.cubic, 30)
 
-    player = EntityBuilder.player(world, Vector2(WIDTH / 2, HEIGHT - 300), camera)
-    player:getComponent("BodyComponent").velocity.y = -700
-
-    CompositionManager.addEntity(player)
+    startTimer:restart()
 end
 
 function Game:draw()
@@ -56,7 +74,6 @@ function Game:draw()
     love.graphics.setCanvas()
 
     -- Post processing
-    -- Bloom.bloomAmount = 0.01
     local result = screenCanvas
     result = Bloom:onPostRender(nil, result)
     result = HDR:onPostRender(nil, screenCanvas)
@@ -64,11 +81,11 @@ function Game:draw()
 
     love.graphics.draw(result)
 
+    CompositionManager.broadcastToAllComponents("uiDraw")
+
     Utils.setFont(15)
     love.graphics.print(("Height: %d"):format(-gameData.maxHeight), 0, 40)
     love.graphics.print(gameData.timeSpeed, 0, 80)
-
-
 end
 
 function Game:update(dt)
@@ -76,25 +93,30 @@ function Game:update(dt)
 
     CompositionManager.broadcastToAllComponents("update", dt * gameData.timeSpeed)
     camera:update(math.min(dt, 1))
+    startTimer:update(dt)
 
-    local bounds = camera:getBounds()
-    local playerTransform = player:getComponent("Transform2dComponent") --[[@as Transform2dComponent]]
-    local playerDamageable = player:getComponent("DamageableComponent") --[[@as DamageableComponent]]
+    uiFadein = math.min(uiFadein + (gameData.gameStarted and dt or 0), 1)
 
-    gameData.maxHeight = math.min(gameData.maxHeight, playerTransform.position.y + HEIGHT/4)
-    camera.position.y = gameData.maxHeight
-
-    if playerDamageable.health <= 0 then
-        Game:enter()
-    end
-
-
-    -- Spawn enemies
-    if gameData.maxHeight < gameData.enemySpawn then
-        local enemyPos = Vector2(math.random(0, WIDTH-32), bounds.topLeft.y - 32)
-        CompositionManager.addEntity(EntityBuilder.enemy(world, enemyPos, player, camera))
-
-        gameData.enemySpawn = gameData.enemySpawn - 500
+    if gameData.gameStarted then
+        local bounds = camera:getBounds()
+        local playerTransform = player:getComponent("Transform2dComponent") --[[@as Transform2dComponent]]
+        local playerDamageable = player:getComponent("DamageableComponent") --[[@as DamageableComponent]]
+        
+        gameData.maxHeight = math.min(gameData.maxHeight, playerTransform.position.y + HEIGHT/4)
+        camera.position.y = gameData.maxHeight
+        
+        if playerDamageable.health <= 0 then
+            Game:enter()
+        end
+        
+        
+        -- Spawn enemies
+        if gameData.maxHeight < gameData.enemySpawn then
+            local enemyPos = Vector2(math.random(0, WIDTH-32), bounds.topLeft.y - 32)
+            CompositionManager.addEntity(EntityBuilder.enemy(world, enemyPos, player, camera))
+            
+            gameData.enemySpawn = gameData.enemySpawn - 500
+        end
     end
 
     -- Time slow
@@ -105,10 +127,18 @@ end
 
 function Game:keypressed(key)
     CompositionManager.broadcastToAllComponents("keypressed", key)
+
+    if not gameData.gameStarted then
+        startTimer:play()
+    end
 end
 
 function Game:mousepressed(x, y, button)
     CompositionManager.broadcastToAllComponents("mousepressed", x, y, button)
+
+    if not gameData.gameStarted then
+        startTimer:play()
+    end
 end
 
 function Game:wheelmoved(x, y)
