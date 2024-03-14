503a82b5c7bf3990c4f5aadd40b0efeff7409f8c
FloatingBanana <thalesmaiademoraes13@gmail.com>
2024-03-14 01:59:38 -0300

    Removed DearImgui

--- src/main.lua
+++ src/main.lua
@@ -5,7 +5,6 @@ end
 
 -- Misc
 local GS = require "libs.gamestate"
-local Imgui = require "libs.cimgui"
 local Utils = require "engine.misc.utils"
 local InputHelper = require "engine.misc.inputHelper"
 local TransitionManager = require "engine.transitions.transitionManager"
@@ -14,8 +13,6 @@ local Game = require "states.GameComposition"
 local Splash = require "states.Splash2"
 
 function love.load(args)
-    Imgui.love.Init()
-
     love.mouse.setGrabbed(true)
 
     InputHelper.registerAxis("horizontal", {"a", "left"}, {"d", "right"})
@@ -31,18 +28,12 @@ function love.draw() ---@diagnostic disable-line: duplicate-set-field
 
     TransitionManager.draw()
 
-    Imgui.Render()
-    Imgui.love.RenderDrawLists()
-
     Utils.setFont(13)
     love.graphics.setColor(1,1,1,1)
     love.graphics.print("FPS: " .. love.timer.getFPS())
 end
 
 function love.update(dt)
-    Imgui.love.Update(dt)
-    Imgui.NewFrame()
-
     TransitionManager.update(dt)
 end
 
@@ -53,68 +44,29 @@ function love.keypressed(key)
         love.event.quit()
     end
 
-    Imgui.love.KeyPressed(key)
-    if Imgui.love.GetWantCaptureKeyboard() then
-        return
-    end
-
     GS.keypressed(key)
 end
 
 function love.keyreleased(key, ...)
-    Imgui.love.KeyReleased(key)
-    if Imgui.love.GetWantCaptureKeyboard() then
-        return
-    end
-
     GS.keyreleased(key, ...)
 end
 
 function love.textinput(t)
-    Imgui.love.TextInput(t)
-    if Imgui.love.GetWantCaptureKeyboard() then
-        return
-    end
-
     GS.textinput(t)
 end
 
 function love.mousemoved(x, y, ...)
-    Imgui.love.MouseMoved(x, y)
-    if Imgui.love.GetWantCaptureMouse() then
-        return
-    end
-
     GS.mousemoved(x, y, ...)
 end
 
 function love.mousepressed(x, y, button, ...)
-    Imgui.love.MousePressed(button)
-    if Imgui.love.GetWantCaptureMouse() then
-        return
-    end
-
     GS.mousepressed(x, y, button, ...)
 end
 
 function love.mousereleased(x, y, button, ...)
-    Imgui.love.MouseReleased(button)
-    if Imgui.love.GetWantCaptureMouse() then
-        return
-    end
-
     GS.mousereleased(x, y, button, ...)
 end
 
 function love.wheelmoved(x, y)
-    Imgui.love.WheelMoved(x, y)
-    if Imgui.love.GetWantCaptureMouse() then
-        return
-    end
-
     GS.wheelmoved(x, y)
-end
-
-function love.quit()
-    return Imgui.love.Shutdown()
 end
